1e311c35fae5647851859a91878738703889d3a9
Manan Jain <mananjain301@gmail.com>
2021-12-21 07:47:44 +0530

    Add gitignore, procfile and update ports

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ const nodemailer = require("nodemailer");
 const { google } = require("googleapis");
 const OAuth2 = google.auth.OAuth2;
 const flash = require('connect-flash');
+const { prototype } = require("nodemailer/lib/sendmail-transport");
 
 const homeStartingContent =
 	"Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -682,6 +683,11 @@ const options = {
 	cert: fs.readFileSync(__dirname + "/security/server.cert"),
 };
 
-https.createServer(options, app).listen(3000, function (req, res) {
-	console.log("Server started at port 3000");
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+https.createServer(options, app).listen(port, function (req, res) {
+	console.log("Server started successfully");
 });
